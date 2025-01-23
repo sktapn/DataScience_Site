a164979e4011fc871e6dc3d5b6757cee24d560d9
neres <144358506+sktapn@users.noreply.github.com>
2025-01-23 15:55:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,28 +7,25 @@ st.set_page_config(
     layout="centered"
 )
 
-# Centralizar apenas o título
-st.markdown(
-    """
-    <h1 style="text-align: center;">🚀 Em Breve!</h1>
-    """, 
-    unsafe_allow_html=True
-)
-
-# Mensagem
-st.write(
-    """
-    Nosso site está em construção e estará disponível em breve! 🚧  
-    Estamos trabalhando para trazer algo incrível para você. Fique ligado! 🔧✨
-    """
-)
-
-# Imagem ou GIF
-st.image(
-    "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif", 
-    caption="Estamos trabalhando para trazer algo incrível!",
-    use_container_width=True
-)
+# Centralizar conteúdo
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    # Título centralizado
+    st.title("🚀 Em Breve!")
+    
+    # Mensagem centralizada
+    st.write(
+        """
+        Nosso site está em construção e estará disponível em breve! 🚧  
+        """
+    )
+    
+    # Imagem centralizada
+    st.image(
+        "https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif", 
+        caption="Estamos trabalhando para trazer algo incrível!",
+        use_container_width=True
+    )
 
 # Rodapé com link
 st.markdown(
